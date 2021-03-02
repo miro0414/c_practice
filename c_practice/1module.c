a3c07293003b9c97ecc110dc2280735470391eda
miro0414 <miro0414@gmail.com>
2021-03-02 14:33:45 +0800

    change the name of module.c

---
+++ c_practice/1module.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include<stdbool.h>
+#define eprintf(...) printf(__VA_ARGS__); fflush(stdout); //for Windows enviroment Git Bash.
+int main(){
+
+
+	return 0;
+}
--- c_practice/module.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define eprintf(...) printf(__VA_ARGS__); fflush(stdout); //for Windows enviroment
-int main(){
-
-
-	return 0;
-}
